3c62e3b05b2fe802209c169ce3578de452eaffa2
= <=>
2024-11-14 17:46:03 -0500

    complete form registrar-persona cab

--- src/app/component/registrar-persona/registrar-persona.component.ts
+++ src/app/component/registrar-persona/registrar-persona.component.ts
@@ -3,11 +3,17 @@ import { PersonaService } from './../../service/persona.service';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxPaginationModule } from 'ngx-pagination';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registrar-persona',
   standalone: true,
-  imports: [CommonModule, NgxPaginationModule],
+  imports: [CommonModule, NgxPaginationModule, ReactiveFormsModule],
   templateUrl: './registrar-persona.component.html',
   styleUrl: './registrar-persona.component.css',
 })
@@ -15,7 +21,31 @@ export class RegistrarPersonaComponent {
   title = 'Registrar Persona';
   personaArray: IPersonaResponse[] = [];
   page: number = 1;
-  constructor(private personaService: PersonaService) {}
+  personaForm: FormGroup;
+
+  constructor(private personaService: PersonaService) {
+    this.personaForm = new FormGroup({
+      idPersona: new FormControl(''),
+      apellidoPaterno: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+      apellidoMaterno: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+      nombres: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+      fechaNacimiento: new FormControl(''),
+      idTipoDocumento: new FormControl('1'),
+      ndocumento: new FormControl(''),
+      direccion: new FormControl(''),
+      idUbigeo: new FormControl('150101'),
+    });
+  }
+
   ngOnInit(): void {
     this.getPersonas();
   }
