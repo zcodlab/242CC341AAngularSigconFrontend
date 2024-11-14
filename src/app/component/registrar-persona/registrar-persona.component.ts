d75759fb76ad8bf6d1014e45d2b51f605b62e5f1
= <=>
2024-11-14 16:48:17 -0500

    add paginacion to registrar-persona

--- src/app/component/registrar-persona/registrar-persona.component.ts
+++ src/app/component/registrar-persona/registrar-persona.component.ts
@@ -2,26 +2,28 @@ import { IPersonaResponse } from '../../model/persona-response';
 import { PersonaService } from './../../service/persona.service';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-registrar-persona',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgxPaginationModule],
   templateUrl: './registrar-persona.component.html',
   styleUrl: './registrar-persona.component.css',
 })
 export class RegistrarPersonaComponent {
-  title = 'RegistrarPersonaComponent';
+  title = 'Registrar Persona';
   personaArray: IPersonaResponse[] = [];
+  page: number = 1;
   constructor(private personaService: PersonaService) {}
   ngOnInit(): void {
     this.getPersonas();
   }
   getPersonas(): void {
     this.personaService.getPersonas().subscribe((result: any) => {
-      console.log('Result', result);
+      //console.log('Result', result);
       this.personaArray = result;
-      console.log(this.personaArray);
+      //console.log(this.personaArray[0]);
     });
   }
 }
