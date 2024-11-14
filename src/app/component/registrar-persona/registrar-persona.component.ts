600a7cb7e47732ce02cfc6112f1e6b7fbdcde6fe
= <=>
2024-11-14 18:36:46 -0500

    add eliminarPersona method

--- src/app/component/registrar-persona/registrar-persona.component.ts
+++ src/app/component/registrar-persona/registrar-persona.component.ts
@@ -9,6 +9,9 @@ import {
   Validators,
   ReactiveFormsModule,
 } from '@angular/forms';
+import { IPersonaRequest } from '../../model/persona-request';
+import Swal from 'sweetalert2';
+import { error } from 'console';
 
 @Component({
   selector: 'app-registrar-persona',
@@ -22,6 +25,7 @@ export class RegistrarPersonaComponent {
   personaArray: IPersonaResponse[] = [];
   page: number = 1;
   personaForm: FormGroup;
+  personaRequest: IPersonaRequest = {} as IPersonaRequest;
 
   constructor(private personaService: PersonaService) {
     this.personaForm = new FormGroup({
@@ -47,6 +51,9 @@ export class RegistrarPersonaComponent {
   }
 
   ngOnInit(): void {
+    this.personaForm.reset();
+    this.personaForm.controls['idTipoDocumento'].setValue(1);
+    this.personaForm.controls['idUbigeo'].setValue(150101);
     this.getPersonas();
   }
   getPersonas(): void {
@@ -56,12 +63,72 @@ export class RegistrarPersonaComponent {
       //console.log(this.personaArray[0]);
     });
   }
+  setPersona(): void {
+    this.personaRequest.idPersona = this.personaForm.get('id_persona')?.value;
+    this.personaRequest.apellidoPaterno =
+      this.personaForm.get('apellidoPaterno')?.value;
+    this.personaRequest.apellidoMaterno =
+      this.personaForm.get('apellidoMaterno')?.value;
+    this.personaRequest.nombres = this.personaForm.get('nombres')?.value;
+    this.personaRequest.fechaNacimiento =
+      this.personaForm.get('fechaNacimiento')?.value;
+    this.personaRequest.idTipoDocumento =
+      this.personaForm.get('idTipoDocumento')?.value;
+    this.personaRequest.ndocumento = this.personaForm.get('ndocumento')?.value;
+    this.personaRequest.direccion = this.personaForm.get('direccion')?.value;
+    this.personaRequest.idUbigeo = this.personaForm.get('idUbigeo')?.value;
+  }
 
+  registrarPersona(): void {
+    console.log('registrando persona');
+    this.setPersona();
+    this.personaService.registrarPersona(this.personaRequest).subscribe(
+      (result: any) => {
+        console.log('registrarPersona', result);
+        this.ngOnInit();
+        Swal.close();
+        Swal.fire({
+          icon: 'success',
+          title: 'registrarPersona....',
+          text: '!Se registro exitosamente la persona!',
+        });
+      },
+      (err: any) => {
+        Swal.close();
+        Swal.fire({
+          icon: 'error',
+          title: 'Advertencia....',
+          text: '!Ah ocurrido un error al registrar Persona',
+        });
+      }
+    );
+  }
   editarPersona(personaResponse: IPersonaResponse): void {
     console.log('editando persona');
   }
 
   eliminarPersona(personaResponse: IPersonaResponse): void {
     console.log('eliminando persona');
+    this.personaRequest.idPersona = personaResponse.idPersona;
+    this.personaService.eliminarPersona(this.personaRequest).subscribe(
+      (result: any) => {
+        console.log('eliminarPersona', result);
+        this.ngOnInit();
+        Swal.close();
+        Swal.fire({
+          icon: 'success',
+          title: 'eliminarPersona....',
+          text: '!Se elimino exitosamente la persona!',
+        });
+      },
+      (err: any) => {
+        Swal.close();
+        Swal.fire({
+          icon: 'error',
+          title: 'Advertencia....',
+          text: '!Ah ocurrido un error al eliminar Persona',
+        });
+      }
+    );
   }
 }
--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BASE_URL } from '../utils/constants';
 import { Observable } from 'rxjs';
+import { IPersonaRequest } from '../model/persona-request';
 
 @Injectable({
   providedIn: 'root',
@@ -12,5 +13,15 @@ export class PersonaService {
 
   getPersonas(): Observable<IPersonaResponse[]> {
     return this.http.get<IPersonaResponse[]>(`${BASE_URL}/persona`);
+  }
+  registrarPersona(persona: IPersonaRequest): Observable<IPersonaResponse> {
+    return this.http.post<IPersonaResponse>(`${BASE_URL}/persona`, persona);
+  }
+
+  eliminarPersona(persona: IPersonaRequest): Observable<IPersonaResponse> {
+    console.log(persona);
+    return this.http.delete<IPersonaResponse>(`${BASE_URL}/persona`, {
+      body: persona,
+    });
   }
 }
