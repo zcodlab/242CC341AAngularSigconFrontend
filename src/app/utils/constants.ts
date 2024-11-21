f956574cf1f937fba2dd3f0de52fa6a0262a2c26
= <=>
2024-11-20 23:11:17 -0500

    set backend to render cloud

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = 'http://localhost:8080/api/v1';
+export const BASE_URL =
+  'https://two42cc341aspringsigconbackendd1.onrender.com/api/v1';
+//'http://localhost:8080/api/v1';
